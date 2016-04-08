a921b5adb2d8b90897348a610780535bc0c802eb
Csaba Kadlecsik <kadlecsik@outlook.com>
2016-04-08 22:28:00 +0200

    Fix more problems

--- homework-1/JEE/JEE-MobileWebshop-ejb/src/main/java/csk/mobilewebshop/exception/IllegalRequestException.java
+++ homework-1/JEE/JEE-MobileWebshop-ejb/src/main/java/csk/mobilewebshop/exception/IllegalRequestException.java
@@ -1,5 +1,8 @@
 package csk.mobilewebshop.exception;
 
+import javax.ejb.ApplicationException;
+
+@ApplicationException
 public class IllegalRequestException extends RuntimeException {
 
     public IllegalRequestException() {
--- homework-1/JEE/JEE-MobileWebshop-ejb/src/main/java/csk/mobilewebshop/exception/ValidationException.java
+++ homework-1/JEE/JEE-MobileWebshop-ejb/src/main/java/csk/mobilewebshop/exception/ValidationException.java
@@ -1,5 +1,8 @@
 package csk.mobilewebshop.exception;
 
+import javax.ejb.ApplicationException;
+
+@ApplicationException
 public class ValidationException extends RuntimeException {
 
     public ValidationException() {
